d7c27de3658665bf02d785ea3abbd02a2fb915b3
hxcl <thehexicle@gmail.com>
2017-08-31 10:23:39 -0700

    initial

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    $('body').on('touchmove', false);
+
+    $('#header').append("http://friedman.bingo");
+
+    shuffle(JSONBingo.squares);
+
+    for (i = 0; i < 24; i++) {
+
+        if (i == 12) {
+            $('#board').append("<div data-value='1' class='square' id='sqfree'><div class='text'><br/>FREE</div></div>");
+            $('#board').append("<div data-value='0' class='square' id='sq12'><div class='text'><br/>" + JSONBingo.squares[i].square + "</div></div>");
+        } else {
+            $('#board').append("<div data-value='0' class='square' id='sq" + i + "'><div class='text'><br/>" + JSONBingo.squares[i].square + "</div></div>");
+        }
+    }
+
+});
+
+shuffle = function(v) {
+    for (var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+    return v;
+};
