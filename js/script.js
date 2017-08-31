be5c01b03a2cb4c75b4fcb44a52ffd6b81fc3b93
hxcl <thehexicle@gmail.com>
2017-08-31 11:14:20 -0700

    organized stuff

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     for (i = 0; i < 24; i++) {
 
         if (i == 12) {
-            $('#board').append("<div data-value='1' class='picture' id='sqfree'><img src='image.jpeg' width='120px' height='120px'></div>");
+            $('#board').append("<div data-value='1' class='picture' id='sqfree'><img src='img/1.jpeg' width='120px' height='120px'></div>");
             $('#board').append("<div data-value='0' class='square' id='sq12'><div class='text'><br/>" + JSONBingo.squares[i].square + "</div></div>");
         } else {
             $('#board').append("<div data-value='0' class='square' id='sq" + i + "'><div class='text'><br/>" + JSONBingo.squares[i].square + "</div></div>");
